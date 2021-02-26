6b0fb05d25389d0bcaef9b2775300c4293824c61
Kevin Shrestha <kevin.g.shrestha@gmail.com>
2021-02-26 12:38:25 -0500

    Initial push. Setup Header and currently working on sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM naming conevetion
+    <div className="app">
+      <h1>Hello world! Let's build Slack using React</h1>
+
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+
+        <Switch>
+
+        </Switch>
+      </div>
+      {/* Sidebar */}
+      {/* React-Router -> Chat screen */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCS2cjqTXIraXehiU_kRzMVcGA16rufako",
+  authDomain: "slack-clone-c3e30.firebaseapp.com",
+  projectId: "slack-clone-c3e30",
+  storageBucket: "slack-clone-c3e30.appspot.com",
+  messagingSenderId: "626304178973",
+  appId: "1:626304178973:web:9768e0209b164c7a39dff4",
+  measurementId: "G-6V8TB3PL7K"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
